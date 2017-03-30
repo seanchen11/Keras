39874045652f595db9cac333cba0fe5af339365e
seanchen11 <103306086@nccu.edu.tw>
2017-03-30 17:07:08 +0800

    123

---
+++ Keras-2.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='Keras',
+      version='2.0.0',
+      description='Deep Learning for Python',
+      author='Francois Chollet',
+      author_email='francois.chollet@gmail.com',
+      url='https://github.com/fchollet/keras',
+      download_url='https://github.com/fchollet/keras/tarball/2.0.0',
+      license='MIT',
+      install_requires=['tensorflow', 'pyyaml', 'six'],
+      extras_require={
+          'h5py': ['h5py'],
+          'visualize': ['pydot-ng'],
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'pytest-xdist'],
+      },
+      packages=find_packages())
